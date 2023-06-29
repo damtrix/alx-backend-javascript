95e6ee780127232e752575c9c1fd3c94a0c91776
damtrix <oludamola.onarinde@gmail.com>
2023-06-29 13:25:43 +0100

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,6 +4,6 @@ export default function initializeRooms() {
   const class1 = new ClassRoom(19);
   const class2 = new ClassRoom(20);
   const class3 = new ClassRoom(34);
-  let new_list = [class1, class2, class3];
-  return new_list;
+  const classList = [class1, class2, class3];
+  return classList;
 }
