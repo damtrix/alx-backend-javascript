d3cdaee39a57396c4fc5d7a9a4aab2437802ce52
damtrix <oludamola.onarinde@gmail.com>
2023-06-29 21:46:51 +0000

    5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,16 @@
 export default class Building {
   constructor(sqft) {
-    if (
-      new.target !== Building &&
-      this.evacuationWarningMessage === undefined
-    ) {
-      throw Error(
-        'Class extending Building must override evacuationWarningMessage'
-      );
-    }
     this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
   }
 }
